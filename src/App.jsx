314b0280e820e928897c9c3e83234eeaae220487
lxry-comet <durdentagirowich@gmail.com>
2026-06-08 16:04:05 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+export default function App() {
+  return (
+    <>
+      {/* Components */}
+      <h1 style={{ 
+        color: '#297516', 
+        fontSize: '24px', 
+        fontFamily: 'Cascadia Mono, sans-serif' ,
+        marginTop: '30%'
+        }}>
+        react-vite_templates
+        </h1>
+    </>
+  );
+};
