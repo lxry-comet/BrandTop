73d6bc76fe34e47dca8f11dfc2672cf1aa5f3a14
lxry-comet <durdentagirowich@gmail.com>
2026-09-08 09:40:12 +0200

    contacts update

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Phone } from 'lucide-react'
 
 //? imports styles
 import css from './Contacts.module.css'
@@ -89,7 +90,7 @@ export class Contacts extends Component {
 								<span>lub</span>
 							</div>
 							<div className={css.contacts__phoneblock}>
-								<span className={css.contacts__phoneicon}>📞</span>
+								<Phone className={css.contacts__phoneicon} size={28} strokeWidth={2} />
 								<div>
 									<a href='tel:+48502725148' className={css.contacts__phonenumber}>
 										+48 502 725 148
